9816e541627def775d83e0f6941b03264e3b75d9
Tanjil Rahman <tanjil.rahman01@yahoo.com>
2021-06-24 11:30:16 +0600

    fixed email sending for production

--- src/pages/api/orderMail.js
+++ src/pages/api/orderMail.js
@@ -8,7 +8,7 @@ export default async (req, res) => {
   let transporter = nodemailer.createTransport({
     host: process.env.EMAIL_SERVER_HOST,
     port: process.env.EMAIL_SERVER_PORT,
-    secure: false, // true for 465, false for other ports
+    secure: process.env.EMAIL_SERVER_PORT === 465 ? true : false, // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL_SERVER_USER,
       pass: process.env.EMAIL_SERVER_PASSWORD,
